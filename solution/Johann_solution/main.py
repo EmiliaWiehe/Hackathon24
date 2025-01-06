27eb2141191698aeef362b48991761ea4859f2e1
Johann4DL <103281292+Johann4DL@users.noreply.github.com>
2025-01-06 19:00:56 +0100

    ML model code okay
    Working ML model, utils.py and main.py. Max code has to be added in main.py, otherwise finished (except requirements.txt)

--- solution/Johann_solution/main.py
+++ solution/Johann_solution/main.py
@@ -2,8 +2,7 @@ import csv
 import os
 import sys
 import random
-from utils import overlay_images_with_transformations, prediction, SingleFolderImageDataset
-from utils import check_tuple_positions, get_dataloader
+from utils import ML_prediction
 from model import SimpleCNN
 import torch
 from torch.utils.data import Dataset, DataLoader
@@ -13,55 +12,42 @@ import torch.nn.functional as F
 
 def main(input_csv, output_path):
 
+    # create results.csv file with headers
+    with open("results.csv", "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(["part", "gripper", "x", "y", "rotation"])
+        print("results.csv created successfully")
+
     # load ML model
     model_dir = "./model"
     os.makedirs(model_dir, exist_ok=True)
     model_path = os.path.join(model_dir, "model.pth")
-    print("Model path: ", model_path)
-
     model = SimpleCNN()
     model.load_state_dict(torch.load(model_path))
     model.eval()
-    print("Model loaded successfully") 
+    print(f"Model loaded successfully from {model_path} and set to evaluation mode") 
 
     # Read the input CSV file
-    #Firstly, it receives a CSV file containing the part and gripper image file names
     with open(input_csv, "r") as f:
         reader = csv.reader(f)
         next(reader)  # Skip the header
+
+        # Loop through each row in the CSV file
         for row in reader:
             part_path = row[0]
             gripper_path = row[1]
             print(f"Part path: {part_path}, Gripper path: {gripper_path}")
-            for i in range(32):
-                # Overlay images with transformations
-                shift_x, shift_y, rotation, image_path = overlay_images_with_transformations(part_path, gripper_path, output_path)
-                print("Images overlayed successfully")
-                # pass combined_image to the model
-                # create a dataset
-                dataset = SingleFolderImageDataset('./result')
-                dataloader = DataLoader(dataset, batch_size=1)
-                print("Dataset and DataLoader created successfully")
-                # make predictions
-                predictions = prediction(model, dataloader)
-                print(predictions)
-                break
 
-            # print("Images overlayed successfully")
 
-            # # get DataLoader
-            # dataloader = get_dataloader(output_path, batch_size=32)
-            # print("Dataloader created successfully")
+            # Max code
+            # analytic_result = max_function(image, output_path)
 
-            # # make predictions
-            # predictions, files = prediction(model, dataloader)
-            # print(predictions)
-            # idx = check_tuple_positions(predictions)
-            #print(f"Predicted class: {predictions[idx][1]}, File name: {predictions[idx][0]}")
-      
+            analytic_result = False  # Set to True if we don't have to continue with the ML model
 
-    # create dataset and dataloader
-
+            # This function calls the Machine Learning model to predict the x, y, and rotation values
+            # The results are printed to the console and written to "results.csv"
+            if analytic_result == False:
+                ML_prediction(part_path, gripper_path, output_path, model)
 
 
 if __name__ == "__main__":
@@ -69,6 +55,7 @@ if __name__ == "__main__":
         print("Usage: python main.py path/to/input/tasks.csv path/to/output/folder", file=sys.stderr)
         sys.exit(1)
     
+    # Get the input CSV file and output folder from the command line arguments
     input_csv = sys.argv[1]
     output_path = sys.argv[2]
     
--- solution/Johann_solution/utils.py
+++ solution/Johann_solution/utils.py
@@ -7,8 +7,9 @@ from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
+import csv
 
-# Function to overlay images with transformations
+# Function to randomly overlay images with transformations
 def overlay_images_with_transformations(part_path, gripper_path, output_path):
 
     # Load images
@@ -30,13 +31,30 @@ def overlay_images_with_transformations(part_path, gripper_path, output_path):
     max_shift_x = -(part.width - gripper.width) // 2  # negative value because we want to shift the gripper to the right
     max_shift_y = -(part.height - gripper.height) // 2 # negative value because we want to shift the gripper down
    
-
     # Random shift values should be negative only and should be less than part width - gripper width
-    shift_x = random.randint(max_shift_x, 0)
-    shift_y = random.randint(max_shift_y, 0)
-    rotation = random.randint(0, 360)
+    shift_x = 0
+    for i in range(2):
+        temp = random.randint(max_shift_x, 0)
+        if temp < shift_x:
+            shift_x = temp
+
+    shift_y = 0
+    for i in range(2):
+        temp = random.randint(max_shift_y, 0)
+        if temp < shift_y:
+            shift_y = temp
+
+    rotation = 0
+    for i in range(2):
+        temp = random.randint(0, 360)
+        if temp > rotation:
+            rotation = temp
+    # shift_x = random.randint(max_shift_x, 0)
+    # shift_y = random.randint(max_shift_y, 0)
+    # rotation = random.randint(0, 360)
     shift_x = start_x + max(min(shift_x, -max_shift_x), max_shift_x)
     shift_y = start_y + max(min(shift_y, -max_shift_y), max_shift_y)
+
     # Transformations to apply
     transformations = [
         {'type': 'rotate', 'value': rotation},
@@ -67,53 +85,113 @@ def overlay_images_with_transformations(part_path, gripper_path, output_path):
 
     return shift_x, shift_y, rotation, output_path
 
+# class SingleFolderImageDataset(Dataset):
+#     def __init__(self, folder_path, transform=None):
+#         self.folder_path = folder_path
+#         self.transform = transform
+#         self.image_files = [f for f in os.listdir(folder_path) if f.endswith(('.png'))]
+#         self.label = 2
 
-def prediction(model, dataloader):
-    model.eval()
-    # files = []
-    predictions = []
-    for images, file_names in dataloader:
-        outputs = model(images)
-        #print(outputs)
-        _, predicted = torch.max(outputs, 1)
-        # files.append(file_names)
-        predictions.append(predicted)
-    return predictions #, files
+#     def __len__(self):
+#         return len(self.image_files)
 
-class SingleFolderImageDataset(Dataset):
-    def __init__(self, folder_path, transform=None):
-        self.folder_path = folder_path
+#     def __getitem__(self, idx):
+#         img_name = self.image_files[idx]
+#         img_path = os.path.join(self.folder_path, img_name)
+#         image = Image.open(img_path).convert("RGB")
+#         image = transforms.Resize((128, 128))(image)
+#         image = transforms.ToTensor()(image)
+        
+#         if self.transform:
+#             image = self.transform(image)
+        
+#         return image, self.label, img_name
+    
+class OneImageDataset(Dataset):
+    def __init__(self, image_path, transform=None):
+        self.image_path = image_path
         self.transform = transform
-        self.image_files = [f for f in os.listdir(folder_path) if f.endswith(('.png'))]
+        self.label = 2
 
     def __len__(self):
-        return len(self.image_files)
+        return 1
 
     def __getitem__(self, idx):
-        img_name = self.image_files[idx]
-        img_path = os.path.join(self.folder_path, img_name)
-        image = Image.open(img_path).convert("RGB")
-        # convert the image to tensor
-        # resizes the image to 128x128
+        image = Image.open(self.image_path).convert("RGB")
         image = transforms.Resize((128, 128))(image)
         image = transforms.ToTensor()(image)
         
         if self.transform:
             image = self.transform(image)
         
-        return image, img_name
+        return image, self.label, self.image_path
     
+        
+def evaluate_model(model, test_dataloader):
+    model.eval()
+    all_predictions = []
+    all_labels = []
+    all_filenames = []
 
-def get_dataloader(folder_path, batch_size=32, shuffle=True, transform=None):
-    dataset = SingleFolderImageDataset(folder_path, transform=transform)
-    print("Dataset created successfully")
-    dataloader = DataLoader(dataset, batch_size=batch_size)
-    return dataloader
+    with torch.no_grad():
+        for images, labels, filenames in test_dataloader:
+            outputs = model(images)
+            _, predicted = torch.max(outputs, 1)
+            all_predictions.extend(predicted.numpy())
+            all_labels.extend(labels.numpy())
+            all_filenames.extend(filenames)
 
-def check_tuple_positions(tuples_list):
-    for idx, tpl in enumerate(tuples_list):
-        if tpl[1] == 1:
-            return idx
+    return all_predictions, all_labels, all_filenames
+
+# def extract_from_string(s, substring_1, substring_2):
+#     start_index = s.find(substring_1)
+#     stop_index = s.find(substring_2)
+#     if start_index == -1:
+#         return None  # Substring not found
+#     s = s[start_index:stop_index]
+#     #remove non numeric characters
+#     s = ''.join(filter(str.isdigit, s))
+#     return int(s)
+
+def ML_prediction(part_path, gripper_path, output_path, model):
+    # with open(input_csv, "r") as f:
+    #     reader = csv.reader(f)
+    #     next(reader)  # Skip the header
+    #     for row in reader:
+    #         part_path = row[0]
+    #         gripper_path = row[1]
+    print(f"Part path: {part_path}, Gripper path: {gripper_path}")
+    temp = False
+    while not temp:   
+        # Overlay images with transformations
+        shift_x, shift_y, rotation, image_path = overlay_images_with_transformations(part_path, gripper_path, output_path)
+
+        # pass combined_image to the model and create a dataset
+        # temp_dataset = SingleFolderImageDataset('./result')
+        temp_dataset = OneImageDataset(image_path)
+        temp_dataloader = DataLoader(temp_dataset, batch_size=1)
+
+        # make predictions
+        prediction, labels, filenames = evaluate_model(model, temp_dataloader)
+        # print(prediction[0], prediction, labels, filenames)
+        # for filename, prediction, label in zip(filenames, predictions, labels):
+        if prediction[0] == 1:
+            print(shift_x, shift_y, rotation)
+            # write the results to a CSV file
+            with open("results.csv", "a") as f:
+                writer = csv.writer(f)
+                writer.writerow([part_path, gripper_path, shift_x, shift_y, rotation])
+                print("Results written to results.csv")
+                # empty the output_path folder after writing the results to the CSV file
+                # image = Image.open(image_path)
+                # image.show()
+                # for file in os.listdir(output_path):
+                #     file_path = os.path.join(output_path, file)
+                #     os.remove(file_path)
+            temp = True
+            break
+                            
+
         
 
 
